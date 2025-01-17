5dce8984054fd1ddeaf575bc3a87cbb03caf4536
AshwinBinuAbraham-UST <286969@ust.com>
2025-01-17 11:51:19 +0530

    'test3'

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <component :is="layout">
+    <router-view />
+  </component>
+  <DialogsWrapper />
+</template>
+<scr"+"ipt  lang="ts" setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { DialogsWrapper } from 'vuejs-confirm-dialog';
+const defaultLayout = 'default'
+const { currentRoute } = useRouter()
+const layout = computed(() => `${currentRoute.value.meta.layout || defaultLayout}-layout`)
+</script>
