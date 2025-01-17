6e5b2e8cae9b25b45030da7be74302d1fcec0fe7
AshwinBinuAbraham-UST <286969@ust.com>
2025-01-17 11:36:03 +0530

    'test2'

--- src/stores/store.ts
+++
@@ -1,8 +0,0 @@
-import { ref} from 'vue'
-import { defineStore } from 'pinia'
-import type { User } from '@/constants/types'
-
-export const useStoreInfo = defineStore('storeUser', () => {
-  const userData = ref<User>()
-  return { userData}
-})
